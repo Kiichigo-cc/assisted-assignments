f9134933d659ad129385a5732ce65c2e74ec57ab
CollinK23 <kimballcollin@gmail.com>
2025-01-18 21:06:24 -0800

    React router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Chatbot from "@/components/Chatbot";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/Sidebar";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <SidebarProvider
+        style={{
+          "--sidebar-width": "10rem",
+          "--sidebar-width-mobile": "10rem",
+        }}
+      >
+        <AppSidebar />
+        <div className="w-full p-8">
+          <Routes>
+            <Route path="/" element={<div>Dashboard</div>} />
+            <Route path="/courses" element={<div>Courses</div>} />
+            <Route path="/chatbot" element={<Chatbot />} />
+            <Route path="/settings" element={<div>Settings</div>} />
+          </Routes>
+        </div>
+      </SidebarProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
